a307c04f92571d52a20a5c89986b65368de989df
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2026-01-25 09:00:26 +0530

    Added all the pages and pricing details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.tsx'
+import Pricing from './pages/Pricing.tsx'
+import Projects from './pages/Projects.tsx'
+import MyProject from './pages/MyProject.tsx'
+import Preview from './pages/Preview.tsx'
+import Community from './pages/Community.tsx'
+import View from './pages/View.tsx'
+import Navbar from "./components/Navbar.tsx";
+
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/pricing' element={<Pricing/>}></Route>
+                <Route path='/projects/:projectId' element={<Projects/>}></Route>
+                <Route path='/projects' element={<MyProject/>}></Route>
+                <Route path='/preview/:projectId' element={<Preview/>}></Route>
+                <Route path='/preview/:projectId/:versionId' element={<Preview/>}></Route>
+                <Route path='community' element={<Community/>}></Route>
+                <Route path='/view/:projectId' element={<View/>}></Route>
+            </Routes>
+
+        </div>
+
+
+    )
 }
 
 export default App
