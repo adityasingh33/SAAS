aae775f43d68421ae1e36a82fa6f43fc6ede8f19
Aditya Singh <adityasinghbharadwaj688@gmail.com>
2026-04-14 22:37:11 +0530

    created

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const View = () => {
+const App = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const View = () => {
   )
 }
 
-export default View
+export default App
